6d3eb7fab2b6aef1119638c8538ade7ed1c5149f
Eugene Skachkov <zhenyfaceit@mail.ru>
2026-01-15 03:05:55 +0300

    Change chat date format to US format

--- frontend-nextjs/src/views/apps/chat/ChatContent.jsx
+++ frontend-nextjs/src/views/apps/chat/ChatContent.jsx
@@ -55,12 +55,13 @@ const ScrollWrapper = ({ children, isBelowLgScreen, scrollRef, className }) => {
 const formatDate = (dateString) => {
   if (!dateString) return ''
   const date = new Date(dateString)
-  return date.toLocaleString('ru-RU', {
-    day: '2-digit',
+  return date.toLocaleString('en-US', {
     month: '2-digit',
+    day: '2-digit',
     year: 'numeric',
     hour: '2-digit',
-    minute: '2-digit'
+    minute: '2-digit',
+    hour12: true
   })
 }
 
